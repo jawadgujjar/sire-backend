2f45536d7f3e96f9186927946af1e0fdb513d73c
usamaahma <usamaahmad087@gmail.com>
2025-02-13 22:28:05 +0500

    cloudinary

--- src/models/index.js
+++ src/models/index.js
@@ -4,3 +4,4 @@ module.exports.Perk = require('./sireperks.model');
 module.exports.GetQuote = require('./getquote.model');
 module.exports.Category = require('./category.model');
 module.exports.Portfolio = require('./portfolio.model');
+module.exports.Product = require('./products.model');
