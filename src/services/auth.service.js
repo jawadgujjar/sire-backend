60f0512be2ef7c1e19760a965511331ddcf69539
usamaahma <usamaahmad087@gmail.com>
2025-06-06 00:24:53 +0500

    changings

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -11,10 +11,10 @@ const { tokenTypes } = require('../config/tokens');
  * @param {string} password
  * @returns {Promise<User>}
  */
-const loginUserWithEmailAndPassword = async (email, password, role) => {
+const loginUserWithEmailAndPassword = async (email, password) => {
   const user = await userService.getUserByEmail(email);
-  if (!user || !(await user.isPasswordMatch(password)) || user.role !== role) {
-    throw new ApiError(httpStatus.UNAUTHORIZED, 'Incorrect email, password, or role');
+  if (!user || !(await user.isPasswordMatch(password))) {
+    throw new ApiError(httpStatus.UNAUTHORIZED, 'Incorrect email or password');
   }
   return user;
 };
